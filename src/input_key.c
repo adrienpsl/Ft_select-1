e291ab4c254705dbe41030abc86eba7c589aecf9
Ant0wan <abarthel@student.42.fr>
2019-09-10 08:37:00 -0400

    42norm on the go(splitting files for 42normn)

--- src/input_key.c
+++ src/input_key.c
@@ -17,54 +17,69 @@
 #include "ft_select.h"
 #include "libtc.h"
 
+static void	key_up(struct s_select *list, struct s_display *display, int *position)
+{
+	list[*position].isunderline = 0;
+	*position = (*position - 1) % display->nb_element;
+	if (*position < 0)
+		*position = display->nb_element - 1;
+	list[*position].isunderline = 1;
+}
+
+static void	key_down(struct s_select *list, struct s_display *display, int *position)
+{
+	list[*position].isunderline = 0;
+	*position = (*position + 1) % display->nb_element;
+	list[*position].isunderline = 1;
+}
+
+static void	key_right(struct s_select *list, struct s_display *display, int *position)
+{
+	if (display->nb_element <= display->wrow)
+		return ;
+	list[*position].isunderline = 0;
+	if ((*position + display->wrow) % display->nb_element < *position)
+		*position = list[*position].row;
+	else
+		*position = *position + display->wrow;
+	list[*position].isunderline = 1;
+}
+
+static void	key_left(struct s_select *list, struct s_display *display, int *position)
+{
+	if (display->nb_element <= display->wrow)
+		return ;
+	list[*position].isunderline = 0;
+	if (list[*position].col == 0)
+	{
+		if (list[*position].row <= display->nb_element % display->wrow)
+			*position = display->nb_element - (display->nb_element % display->wrow) + list[*position].row;
+		else
+			*position = display->nb_element - (display->wrow - *position) - (display->nb_element % display->wrow);
+		if (*position >= display->nb_element)
+			*position -= display->wrow;
+	}
+	else
+		*position = *position - display->wrow;
+	list[*position].isunderline = 1;
+}
+
+static void	key_space(struct s_select *list, struct s_display *display, int *position)
+{
+	list[*position].isselected ^= 1;
+	key_down(list, display, position);
+}
+
 void	key_dispatcher(int key, struct s_select *list, struct s_display *display, int *position)
 {
 	if (key == space)
-	{
-		list[*position].isselected ^= 1;
-		key = down;
-	}
+		key_space(list, display, position);
 	if (key == down)
-	{
-		list[*position].isunderline = 0;
-		*position = (*position + 1) % display->nb_element;
-		list[*position].isunderline = 1;
-	}
+		key_down(list, display, position);
 	else if (key == up)
-	{
-		list[*position].isunderline = 0;
-		*position = (*position - 1) % display->nb_element;
-		if (*position < 0)
-			*position = display->nb_element - 1;
-		list[*position].isunderline = 1;
-	}
+		key_up(list, display, position);
 	else if (key == right)
-	{
-		if (display->nb_element <= display->wrow)
-			return ;
-		list[*position].isunderline = 0;
-		if ((*position + display->wrow) % display->nb_element < *position)
-		       *position = list[*position].row;
-		else
-			*position = *position + display->wrow;
-		list[*position].isunderline = 1;
-	}
+		key_right(list, display, position);
 	else if (key == left)
-	{
-		if (display->nb_element <= display->wrow)
-			return ;
-		list[*position].isunderline = 0;
-		if (list[*position].col == 0)
-		{
-			if (list[*position].row <= display->nb_element % display->wrow)
-				*position = display->nb_element - (display->nb_element % display->wrow) + list[*position].row;
-			else
-				*position = display->nb_element - (display->wrow - *position) - (display->nb_element % display->wrow);
-			if (*position >= display->nb_element)
-				*position -= display->wrow;
-		}
-		else
-			*position = *position - display->wrow;
-		list[*position].isunderline = 1;
-	}
+		key_left(list, display, position);
 }
