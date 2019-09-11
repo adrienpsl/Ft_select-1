b581fb34d18e7f7d753c253c7e6b7627dbdc6c5d
Ant0wan <abarthel@student.42.fr>
2019-09-11 18:46:02 +0200

    get_window_size fix2(ioctl vs tgetnum): replace tgetnum by ioctl cause tgetnum does not react to windows resizing

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 16:16:37 by abarthel          #+#    #+#             */
-/*   Updated: 2019/09/11 18:40:14 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/09/11 18:45:38 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,11 @@
 #include "ft_select.h"
 #include "libtc.h"
 
-#include "libft.h"
-
 void	change_winsize(int sig)
 {
-	static int i;
-
-	++i;
 	(void)sig;
 	tc_clear();
 	get_window_info(g_display);
-	g_display->colsize = g_display->wrow;
 	checkfits(g_display, g_display->nb_element);
 	display_list(g_list, g_display);
 }
