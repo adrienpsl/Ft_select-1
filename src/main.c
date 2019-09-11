edfc77009c1b6567f3bacb90645dc52242e1d641
Ant0wan <abarthel@student.42.fr>
2019-09-11 14:51:53 +0200

    change osx display(main.c): adapt to osx display the termcaps calls

--- src/main.c
+++ src/main.c
@@ -6,10 +6,11 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 16:44:41 by abarthel          #+#    #+#             */
-/*   Updated: 2019/09/11 12:58:37 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:50:54 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include <curses.h>
 #include <unistd.h>
 #include <termcap.h>
@@ -41,11 +42,20 @@ static int	fill_select_struct(struct s_select *list, char **argv, int argc)
 static void	initialize_select_var(int *key, int *position,
 		struct s_display *display)
 {
+	char		*ttyname_s;
+	extern int	g_tc_fd;
+
+	if (!(ttyname_s = ttyname(STDIN_FILENO)))
+		exit(1);
+	else if ((g_tc_fd = open(ttyname_s, O_RDWR)) == -1)
+		exit(1);
 	*key = 0;
 	*position = 0;
 	*display = (struct s_display){.wcol = 0, .wrow = 0, .colsize = 0,
 									.nb_element = 0};
 }
+
+#ifdef __unix__
 
 int			ft_select(int argc, char **argv)
 {
@@ -76,10 +86,42 @@ int			ft_select(int argc, char **argv)
 	return (0);
 }
 
+#else
+
+int			ft_select(int argc, char **argv)
+{
+	struct s_select		list[argc - 1];
+	struct s_display	display;
+	int					position;
+	int					key;
+
+	initialize_select_var(&key, &position, &display);
+	display.nb_element = fill_select_struct(list, argv, argc);
+	get_window_info(&display);
+	get_list_info(&display, list, display.nb_element);
+	checkfits(&display, argc);
+	init_term(1);
+	list[0].isunderline = 1;
+	display_list(list, &display);
+	while ((key = tc_keymove()))
+	{
+		if (key == newline)
+			break ;
+		else
+			key_dispatcher(key, list, &display, &position);
+		display_list(list, &display);
+	}
+	init_term(0);
+	display_selection(list, &display);
+	return (0);
+}
+
+#endif
+
 int			main(int argc, char **argv)
 {
-	ft_printf("%s\n", ttyname(STDOUT_FILENO));
-	return (0);
+	extern int	g_tc_fd;
+
 	if (tc_init())
 		return (1);
 	else if (argc < 2)
@@ -91,5 +133,6 @@ int			main(int argc, char **argv)
 	}
 	else if (ft_select(argc, argv))
 		return (1);
+	close(g_tc_fd);
 	return (0);
 }
